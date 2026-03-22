76c578a59c5ca9a78ca879e4d1444ce6b573fe02
elysia <3302843873@qq.com>
2026-03-22 11:34:40 +0800

    finished the stamp function

--- src/misc.rs
+++ src/misc.rs
@@ -1,3 +1,11 @@
-use crate::types::PackagePaths;
+use crate::{
+    command::{CommandSpec, run},
+    fs_utils::{ensure_dir, touch},
+    types::{PackagePaths, Result},
+};
 
-pub fn stamp(name: &str, path: PackagePaths) {}
+pub fn stamp(name: &str, paths: PackagePaths) -> Result<()> {
+    ensure_dir(&paths.stamp)?;
+    touch(&paths.stamp.join(name))?;
+    Ok(())
+}
